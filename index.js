0adcac8b659019999a900f20be0eb92c2e8ebb4b
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-16 16:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ class Circle{
   }
   set diameter(value){
     this.radius=value;
-    return this.radius*2
+    return this.radius*2;
   }
   
   get circumference(){
-    return Math.PI*this.diameter
+    return Math.PI*this.diameter;
   }
   
   set circumference(value){
-    this.radius=value
-    return `${Math.PI*this.radius*2}`
+    this.radius=value;
+    return Math.PI*this.radius*2;
   }
   
   get area(){
-    return `${Math.PI*this.radius*this.radius}`
+    return Math.PI*this.radius*this.radius;
   }
   
   set area(value){
-    this.radius=value
-    return `${Math.PI*this.radius*this.radius}`
+    this.radius=value;
+    return Math.PI*this.radius*this.radius;
   }
 }
